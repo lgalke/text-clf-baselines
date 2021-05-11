8ca8ed10ec1fb4f8a58a6fbc919157779219cd17
Lukas Galke <git@lpag.de>
2021-05-11 10:41:12 +0200

    Final for Distilbert

--- experiments/eval_distilbert.bash
+++ experiments/eval_distilbert.bash
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 MODEL_TYPE="distilbert"
 MODEL_NAME_OR_PATH="distilbert-base-uncased"
-BATCH_SIZE=128
-GRADIENT_ACCUMULATION_STEPS=1
+BATCH_SIZE=16
+GRADIENT_ACCUMULATION_STEPS=8
 EPOCHS=100
 RESULTS_FILE="results_distilbert.csv"
 
+# Stop on error
+set -e
 for seed in 1 2 3 4 5; do
 	for DATASET in "20ng" "R8" "R52" "ohsumed" "mr"; do
 		python3 run_text_classification.py --model_type "$MODEL_TYPE" --model_name_or_path "$MODEL_NAME_OR_PATH" \
